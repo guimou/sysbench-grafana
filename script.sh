07cdc39d363272b9ed008d511e55ae60d49944fd
Guillaume Moutier <guillaume.moutier@gmail.com>
2021-04-19 22:40:20 -0400

    cleanup

--- script.sh
+++
@@ -1,8 +0,0 @@
-tail -f output.txt | grep -E --line-buffered param_ | while read line; do echo ${line}; done
-
-tail -f /logs/output.txt | \
- grep -E --line-buffered reads | \
- while read line; do \
- echo ${line}; done
-
- tail -f <file> | sed '/string/s/stuff//g' >>
